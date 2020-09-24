6b1ab255d177c2c00b821887c151695dbcae121c
youn yeo won <54175366+dudnjsckrgo@users.noreply.github.com>
2020-09-24 13:31:40 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => { //lambda function notation
+  return (
+    <div>
+      <hi>Hello React!!</hi>
+    </div>
+  );
+}
+
+export default App;
